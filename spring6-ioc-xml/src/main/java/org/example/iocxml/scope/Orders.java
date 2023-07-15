a0ae344c4c3ec87a4f92a685850720148eb8a3b2
panjiasheng <1543139916@qq.com>
2023-07-15 15:01:51 +0800

    Spring6-scope

---
+++ spring6-ioc-xml/src/main/java/org/example/iocxml/scope/Orders.java
@@ -0,0 +1,5 @@
+package org.example.iocxml.scope;
+
+public class Orders {
+
+}
---
+++ spring6-ioc-xml/src/main/java/org/example/iocxml/scope/TestOrders.java
@@ -0,0 +1,16 @@
+package org.example.iocxml.scope;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestOrders {
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean-scope.xml");
+        Orders orders = context.getBean("orders", Orders.class);
+        Orders orders1 = context.getBean("orders", Orders.class);
+        System.out.println(orders);
+        System.out.println(orders1);
+    }
+}
