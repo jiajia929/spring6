f24c6bf9da12e2ab97d2d6796c2eaa8606d5b470
panjiasheng <1543139916@qq.com>
2023-07-15 23:19:25 +0800

    Spring6-动态代理

---
+++ spring6-aop/src/main/java/org/example/spring/aop/example/ProxyFactory.java
@@ -0,0 +1,45 @@
+package org.example.spring.aop.example;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
+public class ProxyFactory {
+
+    //目标对象
+    private Object target;
+
+    public ProxyFactory(Object target) {
+        this.target = target;
+    }
+
+    //返回代理对象
+    public Object getProxy() {
+        /**
+         * Proxy.newProxyInstance()方法
+         * 有三个参数
+         * 1 ClassLoader：加载动态生成代理类的类加载器
+         * 2 Class[] interfaces：目标对象实现的所有接口的class类型数组
+         * 3 InvocationHandler：设置代理对象实现目标对象方法的过程
+         */
+        ClassLoader classLoader = target.getClass().getClassLoader();
+        Class<?>[] interfaces = target.getClass().getInterfaces();
+        //第一个参数：代理对象
+        //第二个参数：需要重写目标对象的方法
+        //第三个参数：method方法里面参数
+        InvocationHandler invocationHandler = new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                //方法调用之前输出
+                System.out.println("[动态代理][日志] "+method.getName()+"，参数："+ Arrays.toString(args));
+                //调用目标的方法
+                Object result = method.invoke(target, args);
+                //方法调用之后输出
+                System.out.println("[动态代理][日志] "+method.getName()+"，结果："+ result);
+                return result;
+            }
+        };
+        return Proxy.newProxyInstance(classLoader,interfaces,invocationHandler);
+    }
+}
---
+++ spring6-aop/src/main/java/org/example/spring/aop/example/TestCal.java
@@ -0,0 +1,11 @@
+package org.example.spring.aop.example;
+
+public class TestCal {
+
+    public static void main(String[] args) {
+        //创建代理对象(动态)
+        ProxyFactory proxyFactory = new ProxyFactory(new CalculatorImpl());
+        Calculator proxy = (Calculator) proxyFactory.getProxy();
+        proxy.add(1,2);
+    }
+}
