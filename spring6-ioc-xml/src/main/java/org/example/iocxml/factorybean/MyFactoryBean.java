075c97b4e2bcc22a61c80667cd032008db9f5554
panjiasheng <1543139916@qq.com>
2023-07-15 15:30:01 +0800

    Spring6-factoryBean

---
+++ spring6-ioc-xml/src/main/java/org/example/iocxml/factorybean/MyFactoryBean.java
@@ -0,0 +1,15 @@
+package org.example.iocxml.factorybean;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class MyFactoryBean implements FactoryBean<User> {
+    @Override
+    public User getObject() throws Exception {
+        return new User();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return User.class;
+    }
+}
---
+++ spring6-ioc-xml/src/main/java/org/example/iocxml/factorybean/User.java
@@ -0,0 +1,4 @@
+package org.example.iocxml.factorybean;
+
+public class User {
+}
