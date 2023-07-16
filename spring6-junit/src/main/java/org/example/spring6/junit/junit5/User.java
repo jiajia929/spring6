9f3446666d16004fbcecffcd1960a42821310512
panjiasheng <1543139916@qq.com>
2023-07-16 11:47:33 +0800

    Spring6-整合Junit

---
+++ spring6-junit/src/main/java/org/example/spring6/junit/junit5/User.java
@@ -0,0 +1,11 @@
+package org.example.spring6.junit.junit5;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+
+    public void run() {
+        System.out.println("user...");
+    }
+}
